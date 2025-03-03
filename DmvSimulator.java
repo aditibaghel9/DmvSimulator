b4875a075405a8ba97270d26776ba6d5565b0d34
aditibaghel9 <asbaghel@quinnipiac.edu>
2025-03-03 17:08:41 -0500

    Miracle Message

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -4,26 +4,41 @@ public static void main(String[] args){
    
     DmvSimulator obj = new DmvSimulator();
 
-    final int randomNum = 200;
+    final int randomNum = 100;
 
     System.out.println("Welcome to the Hamden DMV!");
 
     System.out.println("Here is your number, please wait for your number to be called.");
  
-    int randomNumberInRange = (int)(Math.random() * 200);
+    int randomNumberInRange = (int)(Math.random() * 100);
     System.out.println(randomNumberInRange);
 
     int i = randomNumberInRange+1;
 
+    int chance = (int)(Math.random() * 100);
+
     while(i != randomNumberInRange){
         
-        System.out.println(i);
-        if(i>=200){
-
-            i=0;
-        }
+        System.out.println(i); 
         i++;
+        if(i>=101){
+
+            i=1;
+        }
+       
+
     }
-    System.out.println("Come back when you're not lazy enough to not even bring your documents!");
-}
+
+    if (chance == 1) {
+
+        System.out.println("Good news! You have all your paper work, you are all set my friend!");
+        
+    }
+
+    else{
+
+        System.out.println("Come back when you're not lazy enough to not even bring your documents!");
+    }
+    }
+   
 }
